8c5cd8c9c8f0c6c25118a8dd423c5c4b449cbd02
adiandoyo32 <akhiang32@gmail.com>
2022-02-25 01:51:50 +0700

    fix: cart page ui

--- src/pages/cart/CartPage.tsx
+++ src/pages/cart/CartPage.tsx
@@ -1,7 +1,27 @@
+import { useEffect } from "react"
+import ItemList from "../../components/ItemList"
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { fetchCartList, selectCart } from "../../redux/slice/cart-slice";
+import CartItem from "./components/CartItem";
+
 function CartPage() {
+    const cartState = useAppSelector(selectCart)
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        loadCart();
+    }, [])
+
+    const loadCart = async () => {
+        dispatch(fetchCartList())
+    }
+
     return (
         <div className="flex flex-col">
-            
+            <ItemList
+                data={cartState.cartList}
+                renderItem={(product, index) => <CartItem key={index} product={product} />}
+            />
         </div>
     )
 }
