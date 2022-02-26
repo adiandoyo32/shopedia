805065aeba88080333fe816cf0e9c24b88c832e6
adiandoyo32 <akhiang32@gmail.com>
2022-02-27 02:02:50 +0700

    fix: input field state style

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,14 @@
+import classNames from "classnames";
 import { useState } from "react";
+import { camelize } from "../libs/utils";
 
 interface InputFieldProps {
   label?: string;
   type?: "text" | "number" | "password";
   required?: boolean;
+  value?: string | number
+  onChange?: React.ChangeEventHandler<HTMLInputElement>
+  valid?: boolean
 }
 
 const InputField: React.FC<InputFieldProps> = (props) => {
@@ -13,18 +18,28 @@ const InputField: React.FC<InputFieldProps> = (props) => {
     setValue(e.target.value)
   }
 
+  const inputState = {
+    'border-gray-200': props.valid,
+    'border-red-500': !props.valid,
+    'focus:border-blue-300': props.valid,
+    'focus:border-red-300': !props.valid,
+    'focus:ring-blue-300': props.valid,
+    'focus:ring-red-300': !props.valid
+   }
+
   return (
     <div className="mb-4">
-      <label htmlFor={props.label} className="block mb-2 text-sm font-medium">{props.label}</label>
+      <label htmlFor={camelize(props.label)} className="inline-block mb-1.5 text-sm font-medium">{props.label}</label>
       <input
-        id={props.label}
-        value={value}
+        id={camelize(props.label)}
+        value={props.value}
         type="text"
-        className="w-full block p-2.5 bg-gray-50 border border-gray-200 text-sm rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+        className={classNames(`w-full mb-1.5 block p-2.5 bg-gray-50 border text-sm rounded-md focus:outline-none focus:ring-2 transition`, inputState)}
         placeholder={props.label}
-        onChange={handleChange}
+        onChange={props.onChange}
         required={props.required}
       />
+      {props.children}
     </div>
   );
 };
