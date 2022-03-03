4ce57d294efb64fece9d87724863ac4489d65483
adiandoyo32 <akhiang32@gmail.com>
2022-03-04 01:42:05 +0700

    feat: add custom shadow tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
   theme: {
     minWidth: {
       '2xl': '42rem',
+    },
+    extend: {
+      boxShadow: {
+        'lg-top': '0 -5px 15px -6px rgba(0, 0, 0, 0.1)',
+      }
     }
   },
   plugins: [],
