ec84ec21dacad442c99de34a95171fa16c5fb1e7
adiandoyo32 <akhiang32@gmail.com>
2022-03-28 09:54:51 +0700

    feat: webpack bundle analyzer

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   webpack: {
     alias: {
       "@assets": path.resolve(__dirname, 'src/assets/')
-    }
-  }
+    },
+    plugins: [
+      new BundleAnalyzerPlugin()
+    ]
+  },
 };
